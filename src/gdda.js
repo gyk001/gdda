ba67b7720cc401717e5824050f80e3a49523ae0a
gyk001 <gyk001@gmail.com>
2013-01-05 18:53:21 +0800

    add  jqgrid and highcharts lib
    jqgrid version 4.4.1
    highcharts version 2.3.3

--- src/gdda.js
+++ src/gdda.js
@@ -11,7 +11,8 @@
   // Collection method.
   $.fn.gdda = function() {
     "strict";
-    return this.each(function() {
+    var options = $.extend(this.defaults, options); 
+    return this.each(function(options) {
       $(this).html('gdda');
     });
   };
@@ -21,6 +22,12 @@
     "strict";
     return 'gdda';
   };
+
+  //暴露默认配置,允许运行时更改 
+  $.fn.gdda.defaults={
+    
+  };
+
 
   $.expr[':'].gdda = function(elem) {
     "strict";
